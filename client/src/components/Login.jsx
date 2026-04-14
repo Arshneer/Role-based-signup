ea23224a6c6e63ad2655aea9a7bf91b485e1865c
Arshneer <your-email@gmail.com>
2026-04-15 00:57:25 +0530

    updated project changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
 
   const onSubmit = async (data) => {
     try {
-      const res = await axios.post("http://localhost:5000/login", data);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, data);
 
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("role", res.data.role);
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -19,7 +19,7 @@ export default function Signup() {
 
   const onSubmit = async (data) => {
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, data);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/signup`, data);
 
       setMsg("Signup successful ✅");
 
